1b8147defcff4ff32f43329e771c6031a1c03a3a
Bader <btabeche23@gmail.com>
2021-02-21 21:57:27 +0100

    c

---
+++ index.js
@@ -0,0 +1,32 @@
+document.getElementById('q1').onclick = function changeContent(){
+    document.getElementById('b1').style.fontWeight = 700
+    document.getElementById('hbr1').style.display = 'none'
+    document.getElementById('a1').style.display = 'block'
+    
+}
+
+document.getElementById('q2').onclick = function changeContent(){
+    document.getElementById('b2').style.fontWeight = 700
+    document.getElementById('hbr2').style.display = 'none'
+    document.getElementById('a2').style.display = 'block'
+}
+
+
+document.getElementById('q3').onclick = function changeContent(){
+    document.getElementById('b3').style.fontWeight = 700
+    document.getElementById('hbr3').style.display = 'none'
+    document.getElementById('a3').style.display = 'block'
+}
+
+
+document.getElementById('q4').onclick = function changeContent(){
+    document.getElementById('b4').style.fontWeight = 700
+    document.getElementById('hbr4').style.display = 'none'
+    document.getElementById('a4').style.display = 'block'
+}
+
+document.getElementById('q5').onclick = function changeContent(){
+    document.getElementById('b5').style.fontWeight = 700
+    document.getElementById('hbr5').style.display = 'none'
+    document.getElementById('a5').style.display = 'block'
+}
